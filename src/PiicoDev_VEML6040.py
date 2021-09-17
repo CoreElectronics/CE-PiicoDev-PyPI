407e38d30796fa451f4ad1280438926b5e1252b2
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-09-17 10:36:50 +1000

    Update PiicoDev_VEML6040.py
    wrong file was indexed

--- src/PiicoDev_VEML6040.py
+++ src/PiicoDev_VEML6040.py
@@ -1,61 +1,52 @@
-# Class to read data from the Core Electronics PiicoDev MS5637 Pressure Sensor
-# Ported to MicroPython by Peter Johnston at Core Electronics MAY 2021
-# Original repo https://github.com/TEConnectivity/piweathershield-python/tree/67fa820647fafa5a1a7a5a4828ee13d80a60a279
+# A class to read data from the VEML6040 i2c light sensor
+# Read RGB or HSV data
+# Classify Hue given a dictionary of colour definitions
+
+# No warranties express or implied, including any warranty of merchantability and warranty of fitness for a particular purpose.
+# Written by Peter Johnston and Michael Ruppe at Core Electronics May 2021
 
 from PiicoDev_Unified import *
+from utime import sleep_ms
+from math import sqrt
 
-compat_str = '\nUnified PiicoDev library out of date.  Get the latest module: https://piico.dev/unified \n'
+# Registers
+_veml6040Address = 0x10
+_CONF = b'\x00'
+_REG_RED = b'\x08'
+_REG_GREEN = b'\x09'
+_REG_BLUE = b'\x0A'
+_REG_WHITE = b'\x0B'
 
-class PiicoDev_MS5637(object):
-    # MS5637 device address
-    _I2C_ADDRESS = 0x76
+_DEFAULT_SETTINGS = b'\x00' # initialise gain:1x, integration 40ms, Green Sensitivity 0.25168, Max. Detectable Lux 16496
+                            # No Trig, Auto mode, enabled.
+_SHUTDOWN = b'\x01'         # Disable colour sensor
+_INTEGRATION_TIME = 40      # ms
+_G_SENSITIVITY = 0.25168    # lux/step
 
-    # MS5637 device commands
-    _SOFTRESET = 0x1E
-    _MS5637_START_PRESSURE_ADC_CONVERSION = 0x40
-    _MS5637_START_TEMPERATURE_ADC_CONVERSION = 0x50
-    _MS5637_CONVERSION_OSR_MASK = 0x0F
-    _ADC_READ = 0x00
+_NaN=float('NaN')
 
-    # MS5637 commands read eeprom
-    _MS5637_PROM_ADDR_0 = 0xA0
-    _MS5637_PROM_ADDR_1 = 0xA2
-    _MS5637_PROM_ADDR_2 = 0xA4
-    _MS5637_PROM_ADDR_3 = 0xA6
-    _MS5637_PROM_ADDR_4 = 0xA8
-    _MS5637_PROM_ADDR_5 = 0xAA
-    _MS5637_PROM_ADDR_6 = 0xAC
-
-    # MS5637 commands conversion time
-    _MS5637_CONV_TIME_OSR_256 = 1 # 0.001
-    _MS5637_CONV_TIME_OSR_512 = 2 # 0.002
-    _MS5637_CONV_TIME_OSR_1024 = 3 # 0.003
-    _MS5637_CONV_TIME_OSR_2048 = 5 # 0.005
-    _MS5637_CONV_TIME_OSR_4096 = 9 # 0.009
-    _MS5637_CONV_TIME_OSR_8192 = 17 # 0.017
+def rgb2hsv(r, g, b):
+    r = float(r/65535)
+    g = float(g/65535)
+    b = float(b/65535)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h,s,v = high,high,high
+    d = high - low
+    s = 0 if high == 0 else d/high
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d+(6 if g < b else 0),
+            g: (b - r) / d+2,
+            b: (r - g) / d+4,
+        }[high]
+        h /= 6
+    return {'hue':h*360,'sat':s, 'val':v}
     
-    # MS5637 commands resolution 
-    _RESOLUTION_OSR_256 = 0
-    _RESOLUTION_OSR_512 = 1 
-    _RESOLUTION_OSR_1024 = 2
-    _RESOLUTION_OSR_2048 = 3
-    _RESOLUTION_OSR_4096 = 4
-    _RESOLUTION_OSR_8192 = 5
-    
-    # Coefficients indexes for temperature and pressure computation
-    _MS5637_CRC_INDEX = 0
-    _MS5637_PRESSURE_SENSITIVITY_INDEX = 1 
-    _MS5637_PRESSURE_OFFSET_INDEX = 2
-    _MS5637_TEMP_COEFF_OF_PRESSURE_SENSITIVITY_INDEX = 3
-    _MS5637_TEMP_COEFF_OF_PRESSURE_OFFSET_INDEX = 4
-    _MS5637_REFERENCE_TEMPERATURE_INDEX = 5
-    _MS5637_TEMP_COEFF_OF_TEMPERATURE_INDEX = 6
-    
-    eeprom_coeff = [0,0,0,0,0,0,0,0]
-    
-    coeff_valid = False
-
-    def __init__(self, bus=None, freq=None, sda=None, scl=None, addr = _I2C_ADDRESS):
+class PiicoDev_VEML6040(object):
+    def __init__(self, bus=None, freq=None, sda=None, scl=None, addr = _veml6040Address):
         try:
             if compat_ind >= 1:
                 pass
@@ -65,130 +56,58 @@ class PiicoDev_MS5637(object):
             print(compat_str)
         self.i2c = create_unified_i2c(bus=bus, freq=freq, sda=sda, scl=scl)
         self.addr = addr
-        #try:
-        self.i2c.write8(self.addr, None, bytes([self._SOFTRESET]))
-        sleep_ms(15)
-        #except Exception:
-        #    print('Device 0x{:02X} not found'.format(self.addr))
-
-    # Set  ADC resolution.
-    # res : ms5637_resolution_osr : Resolution requested
-    # return temperature command, pressure command, temperature conversion time, pressure conversion time 
-    def set_resolution(self,res) :
-        time = [self._MS5637_CONV_TIME_OSR_256,
-        self._MS5637_CONV_TIME_OSR_512,
-        self._MS5637_CONV_TIME_OSR_1024,
-        self._MS5637_CONV_TIME_OSR_2048,
-        self._MS5637_CONV_TIME_OSR_4096,
-        self._MS5637_CONV_TIME_OSR_8192]
-        cmd_temp = res *2;
-        cmd_temp |= self._MS5637_START_TEMPERATURE_ADC_CONVERSION;
-        _time_temp = time[int((cmd_temp & self._MS5637_CONVERSION_OSR_MASK)/2)]
-        
-        cmd_pressure = res *2;
-        cmd_pressure |= self._MS5637_START_PRESSURE_ADC_CONVERSION;
-        _time_pressure = time[int((cmd_pressure & self._MS5637_CONVERSION_OSR_MASK)/2)]
-        return cmd_temp,cmd_pressure, _time_temp, _time_pressure
-
-    # Read eeprom coefficients
-    # cmd : address of coefficient in EEPROM
-    def read_eeprom_coeff (self, cmd) :
-        data = self.i2c.readfrom_mem(self.addr, cmd, 2)
-        return int.from_bytes(data, 'big')
-
-    # Reads the ms5637 EEPROM coefficients to store them for computation.
-    # Returns all coefficients read in the EEPROM 
-    def read_eeprom(self) : 
-        a = 0
-        coeffs = [0,0,0,0,0,0,0,0]
-        liste = [self._MS5637_PROM_ADDR_0,
-        self._MS5637_PROM_ADDR_1,
-        self._MS5637_PROM_ADDR_2,
-        self._MS5637_PROM_ADDR_3,
-        self._MS5637_PROM_ADDR_4,
-        self._MS5637_PROM_ADDR_5,
-        self._MS5637_PROM_ADDR_6,]
-    
-        for i in liste :
-            coeffs[a] = self.read_eeprom_coeff(i)
-            a = a+1
-        self.coeff_valid = True
-        return coeffs
-
-    # Triggers conversion and read ADC value
-    # Cmd : Command used for conversion (will determine Temperature vs Pressure and osr)
-    # _time : ms
-    # adc : ADC value
-    def conversion_read_adc(self,cmd,_time) :
-        self.i2c.write8(self.addr, None, bytes([cmd]))
-        sleep_ms(_time)
-        data = self.i2c.readfrom_mem(self.addr, self._ADC_READ, 3)
-        adc = int.from_bytes(data, 'big')
-        return adc
-  
-    # Read Temperature and Pressure, perform compensation
-    # res: resolution [ # ]
-    # returns Temperature [degC] and Pressure [hPa]
-    def read_temperature_and_pressure(self,res=_RESOLUTION_OSR_8192) :
-        if self.coeff_valid == False :
-            self.eeprom_coeff = self.read_eeprom()
-        (cmd_temp, cmd_pressure,_time_temp,_time_pressure) = self.set_resolution(res)
         try:
-            adc_temperature = self.conversion_read_adc(cmd_temp,_time_temp)
-            adc_pressure = self.conversion_read_adc(cmd_pressure,_time_pressure)
+            self.i2c.write8(self.addr, _CONF, _SHUTDOWN)
+            self.i2c.write8(self.addr, _CONF, _DEFAULT_SETTINGS) 
+        except Exception:
+            print('Device 0x{:02X} not found'.format(self.addr))
+            
+    def classifyHue(self, hues={"red":0,"yellow":60,"green":120,"cyan":180,"blue":240,"magenta":300}, min_brightness=0):
+        d=self.readHSV()
+        if d['val'] > min_brightness:
+            key, val = min(hues.items(), key=lambda x: abs(d['hue'] - x[1]))
+            return key
+        else:
+            return 'None'
+    
+    # Read colours from VEML6040
+    # Returns raw red, green and blue readings, ambient light [Lux] and colour temperature [K]
+    def readRGB(self):
+        try:
+            raw_data = self.i2c.read16(self.addr, _REG_RED)        # returns a bytes object   
+            u16red = int.from_bytes(raw_data, 'little')
+            
+            raw_data = (self.i2c.read16(self.addr, _REG_GREEN))    # returns a bytes object
+            u16grn = int.from_bytes(raw_data, 'little')
+            
+            raw_data = (self.i2c.read16(self.addr, _REG_BLUE))     # returns a bytes object
+            u16blu = int.from_bytes(raw_data, 'little')
+            
+            raw_data = (self.i2c.read16(self.addr, _REG_WHITE))    # returns a bytes object
+            data_white_int = int.from_bytes(raw_data, 'little')
         except:
             print(i2c_err_str.format(self.addr))
-            return float('NaN'), float('NaN')
-        if ((type(adc_temperature) is not int) or (type(adc_pressure) is not int)):
-            return float('NaN'), float('NaN')
-         # Difference between actual and reference temperature = D2 - Tref
-        dT = (adc_temperature) - (self.eeprom_coeff[self._MS5637_REFERENCE_TEMPERATURE_INDEX] * 0x100)
-         # Actual temperature = 2000 + dT * TEMPSENS
-        TEMP = 2000 + (dT * self.eeprom_coeff[self._MS5637_TEMP_COEFF_OF_TEMPERATURE_INDEX] >> 23);
-         # Second order temperature compensation
-        if TEMP < 2000 : 
-            T2 = ( 3 * ( dT  * dT  ) ) >> 33
-            OFF2 = 61 * (TEMP - 2000) * (TEMP - 2000) / 16 
-            SENS2 = 29 * (TEMP - 2000) * (TEMP - 2000) / 16 
-            if TEMP < -1500 :
-                OFF2 += 17 * (TEMP + 1500) * (TEMP + 1500) 
-                SENS2 += 9 * ((TEMP + 1500) * (TEMP + 1500))
-        else :
-            T2 = ( 5 * ( dT  * dT  ) ) >> 38
-            OFF2 = 0 
-            SENS2 = 0
-
-        #  OFF = OFF_T1 + TCO * dT
-        OFF = ( (self.eeprom_coeff[self._MS5637_PRESSURE_OFFSET_INDEX]) << 17 ) + ( ( (self.eeprom_coeff[self._MS5637_TEMP_COEFF_OF_PRESSURE_OFFSET_INDEX]) * dT ) >> 6 ) 
-        OFF -= OFF2 ;
-
-        # Sensitivity at actual temperature = SENS_T1 + TCS * dT
-        SENS = ( self.eeprom_coeff[self._MS5637_PRESSURE_SENSITIVITY_INDEX] * 0x10000 ) + ( (self.eeprom_coeff[self._MS5637_TEMP_COEFF_OF_PRESSURE_SENSITIVITY_INDEX] * dT) >> 7 ) 
-        SENS -= SENS2
-        #  Temperature compensated pressure = D1 * SENS - OFF
-        P = ( ( (int(adc_pressure * SENS)) >> 21 ) - int(OFF) ) >> 15 
-
-        temperature = ( TEMP - T2 ) / 100.0
-        pressure = P / 100.0
-
-        return temperature, pressure
+            return {"red":_NaN,"green":_NaN,"blue":_NaN,"white":_NaN,"als":_NaN,"cct":_NaN}
+        
+        # Generate the XYZ matrix based on https://www.vishay.com/docs/84331/designingveml6040.pdf
+        colour_X = (-0.023249*u16red)+(0.291014*u16grn)+(-0.364880*u16blu)
+        colour_Y = (-0.042799*u16red)+(0.272148*u16grn)+(-0.279591*u16blu)
+        colour_Z = (-0.155901*u16red)+(0.251534*u16grn)+(-0.076240*u16blu)
+        colour_total = colour_X+colour_Y+colour_Z
+        if colour_total == 0:
+            return {"red":_NaN,"green":_NaN,"blue":_NaN,"white":_NaN,"als":_NaN,"cct":_NaN}
+        else:
+            colour_x = colour_X / colour_total
+            colour_y = colour_Y / colour_total
+        
+        # Use McCamy formula
+        colour_n = (colour_x - 0.3320)/(0.1858 - colour_y)
+        colour_CCT = 449.0*colour_n ** 3+3525.0*colour_n ** 2+6823.3*colour_n+5520.33
+        
+        # Calculate ambient light in Lux
+        colour_ALS = u16grn*_G_SENSITIVITY
+        return {"red":u16red,"green":u16grn,"blue":u16blu,"white":data_white_int,"als":colour_ALS,"cct":colour_CCT}
     
-    # res: resolution [ # ]
-    # Returns the pressure [hPa]
-    def read_pressure(self,res=_RESOLUTION_OSR_8192) :
-        temperature_and_pressure = self.read_temperature_and_pressure(res)
-        return temperature_and_pressure[1]
-    
-    # Returns the altitude [m]
-    def read_altitude(self,pressure_sea_level=1013.25):
-        pressure = self.read_pressure()
-        return 44330*(1-(float(pressure)/pressure_sea_level)**(1/5.255))
-
-    def close(self):
-        self.i2c.close()
-
-    def __enter__(self):
-        return self        
-
-    def __exit__(self, type, value, traceback):
-        self.close()
+    def readHSV(self):
+        d = self.readRGB()
+        return rgb2hsv(d['red'],d['green'],d['blue'])
